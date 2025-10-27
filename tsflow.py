f484296d4959552c2ec9bf8bb3ce6fee9b0c0ca1
Maxessien <essienmax484@gmail.com>
2025-10-27 17:31:25 +0000

    Updated to simple spam detector

--- tsflow.py
+++ tsflow.py
@@ -2,19 +2,43 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+# Simple spam vs normal email dataset
 
-x = tf.constant([[2.0]], [[5.0]], [[6.0]])
-y = tf.constant([[4.0]], [[10.0]], [[12.0]])
-
-
-model = keras.Sequential([
-    layers.Dense(5, activation="linear", input_shape=(1,))
+# Features:
+# [contains_link, contains_money_words]
+x = tf.constant([
+    [1, 1],  # "Win cash now" -> spam
+    [0, 1],  # "Claim your offer" -> spam
+    [1, 0],  # "Click the link" -> spam
+    [0, 0],  # "Meeting tomorrow" -> normal
+    [0, 0],  # "Lunch at 12?" -> normal
+    [1, 0],  # "Visit my page" -> spam
+    [0, 1],  # "Earn points today" -> spam
+    [0, 0], #Can we talk
+])
+y = tf.constant([
+    [1],
+    [1],
+    [1],
+    [0],
+    [0],
+    [1],
+    [1],
+    [0],
 ])
 
-model.compile(optimizer="adam", loss="mean_squared_error")
+model = keras.Sequential([
+    layers.Dense(5, activation="relu", input_shape=(2,)),
+    layers.Dense(1, activation="sigmoid")
+    ])
 
-model.fit(x, y, epochs=20, batch_size=3, verbose=2, validation_data=None)
+optimizer = keras.optimizers.Adam(learning_rate=0.05)  # or even 0.001
+model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"])
 
 
-print(model.predict([[2.0]]))
+model.fit(x, y, epochs=20, batch_size=1, verbose=2)
 
+print(model.predict(tf.constant([[0, 0]])))
+print(model.predict(tf.constant([[1, 0]])))
+print(model.predict(tf.constant([[1, 1]])))
+print(model.predict(tf.constant([[0, 1]])))
