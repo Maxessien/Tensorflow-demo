84fbc1dcf6e306657f21431eb0d839cb3118d9a4
Max Essien <essienmax484@gmail.com>
2025-12-30 14:54:34 +0000

    Just ran some tests

--- tsflow.py
+++ tsflow.py
@@ -1,36 +1,41 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
 
 # Simple spam vs normal email dataset
 
 # Features:
 # [contains_link, contains_money_words]
-x = tf.constant([
-    [1, 1],  # "Win cash now" -> spam
-    [0, 1],  # "Claim your offer" -> spam
-    [1, 0],  # "Click the link" -> spam
-    [0, 0],  # "Meeting tomorrow" -> normal
-    [0, 0],  # "Lunch at 12?" -> normal
-    [1, 0],  # "Visit my page" -> spam
-    [0, 1],  # "Earn points today" -> spam
-    [0, 0], #Can we talk
-])
-y = tf.constant([
-    [1],
-    [1],
-    [1],
-    [0],
-    [0],
-    [1],
-    [1],
-    [0],
-])
+x = tf.constant(
+    [
+        [1, 1],
+        [0, 1],
+        [1, 0],
+        [0, 0],
+        [0, 0],
+        [1, 0],
+        [0, 1],
+        [0, 0], 
+    ]
+)
+y = tf.constant(
+    [
+        [1],
+        [1],
+        [1],
+        [0],
+        [0],
+        [1],
+        [1],
+        [0],
+    ]
+)
 
-model = keras.Sequential([
-    layers.Dense(5, activation="relu", input_shape=(2,)),
-    layers.Dense(1, activation="sigmoid")
-    ])
+model = keras.Sequential(
+    [
+        keras.layers.Dense(5, activation="relu", input_shape=(2,)),
+        keras.layers.Dense(1, activation="sigmoid"),
+    ]
+)
 
 optimizer = keras.optimizers.Adam(learning_rate=0.05)  # or even 0.001
 model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"])
